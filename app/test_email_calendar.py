a519fe85ae167953aa4a5ea8e2294260970595eb
sonia-br <snbrmaej@gmail.com>
2025-06-30 14:16:37 +0200

    feat: move run.py for email feature to a separate file after the merge

---
+++ app/test_email_calendar.py
@@ -0,0 +1,15 @@
+# temporarily moved here run.py content from email feature 
+# we can add this code to routes later 
+
+from send_email import send_confirmation_email
+from create_event import get_access_token, create_calendar_event
+
+if __name__ == "__main__":
+    user_email = "user@example.com"
+    booking_info = "Booking: May 20, 2025 at 2 PM"
+    start = "2025-05-20T14:00:00"
+    end = "2025-05-20T15:00:00"
+
+    send_confirmation_email(user_email, booking_info)
+    token = get_access_token()
+    create_calendar_event(token, user_email, "Booking with You", start, end)
