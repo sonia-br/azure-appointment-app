dd52ed126c69b8dddc5fadc08f85e219cfdf6a8d
sonia-br <snbrmaej@gmail.com>
2025-07-04 02:47:08 +0200

    fix connection to azure db

--- app/models.py
+++ app/models.py
@@ -5,11 +5,13 @@ from sqlalchemy.dialects.mssql import BIT
 db = SQLAlchemy()
 
 class Slot(db.Model):
+    __tablename__ = 'slots'  # explicitly match the table name
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.DateTime, nullable=False)
     available = db.Column(db.Integer, default=1, nullable=False)
 
 class Appointment(db.Model):
+    __tablename__ = 'appointments'  # explicitly match the table name
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False)
